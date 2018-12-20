e09b6b7ccb3d2b46d366d12e17230c803ca77d11
itxiaolong3 <1710600973@qq.com>
2018-12-20 19:19:56 +0800

    首次上传
    1

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "百货商城",
+    uniacid: "109",
+    acid: "109",
+    multiid: "0",
+    version: "2.7.6",
+    siteroot: "https://pj.dede1.com/app/index.php",
+    design_method: "3"
+};
